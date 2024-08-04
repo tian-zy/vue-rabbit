95e591537e957882cfdc09b449dff13429da4f74
tiantian <2296493794@qq.com>
2024-08-04 10:58:25 +0800

    Layout静态结构搭建

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,13 @@
 <template>
   <!-- 一级路由出口组件 -->
    <router-view></router-view>
+   <div class="test">
+    test 
+   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test {
+  color: $xtxColor
+}
 </style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>首页</div>
+  <LayoutNav></LayoutNav>
+  <LayoutHeader></LayoutHeader>
   <!-- 二级路由出口 -->
    <router-view></router-view>
+   <LayoutFooter></LayoutFooter>
 </template>
