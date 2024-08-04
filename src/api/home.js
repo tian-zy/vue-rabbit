3d7e0668abf9001aac468663552766c065c2978b
tiantian <2296493794@qq.com>
2024-08-04 23:55:35 +0800

    新鲜好物和人气推荐模块业务实现

--- src/api/home.js
+++ src/api/home.js
@@ -2,3 +2,9 @@ import instance from "@/utils/request"
 
 //获取轮播图
 export const getBannerAPI = () => instance.get('home/banner')
+
+//新鲜好物
+export const findNewAPI = () => instance.get('home/new')
+
+// 人气推荐
+export const getHotAPI = () => instance.get('home/hot')
