15cfa22db75b5d05ab2d6c9ddedf522bb80d7119
tiantian <2296493794@qq.com>
2024-08-07 12:10:41 +0800

    401拦截处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user';
+import router from "@/router";
 
 // 创建实例时配置默认值
 const instance = axios.create({
@@ -31,10 +32,19 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
+  // 登录失败的情况
   ElMessage({
     type: 'warning',
     message: error.response.data.message
   })
+  // 401 token 失效处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  const userStore = useUserStore()
+  if(error.response.status === 401) {
+    userStore.removeUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(error);
 });
 
