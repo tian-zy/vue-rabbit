6b06edd42d09416d5b2b12242cdd4ddc74f71a7c
tiantian <2296493794@qq.com>
2024-08-09 11:40:51 +0800

    支付功能实现

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,8 @@
 <template>
   <!-- 一级路由出口组件 -->
    <router-view></router-view>
-   <div class="test">
-    test 
-   </div>
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $xtxColor
-}
+
 </style>
