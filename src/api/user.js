25ec3f6c925e103d94ed099f787968e736354cfe
tiantian <2296493794@qq.com>
2024-08-09 21:06:29 +0800

    会员中心-个人信息数据渲染

--- src/api/user.js
+++ src/api/user.js
@@ -2,3 +2,6 @@ import instance from "@/utils/request"
 
 //登录
 export const loginAPI = ({ account, password }) => instance.post('/login', { account, password })
+
+// 个人中心 猜你喜欢
+export const getLikeListAPI = ({ limit = 4 }) => instance.get('/goods/relevant', { params: { limit } })
