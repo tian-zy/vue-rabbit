a1ca06e38eff0a59e86098b8fbc955df339f7044
tiantian <2296493794@qq.com>
2024-08-10 12:00:19 +0800

    会员中心-订单-分页功能

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -15,6 +15,7 @@ const tabTypes = [
 
 // 获取订单列表
 const orderList = ref([])
+const total = ref(0)
 const params = ref({
   orderState: 0,
   page: 1,
@@ -23,13 +24,20 @@ const params = ref({
 const getOrderList = async () => {
   const res = await getUserOrder(params.value)
   orderList.value = res.result.items
-  // total.value = res.result.counts
+    // 存入总条数
+  total.value = res.result.counts
 }
 onMounted(() => getOrderList())
 
 // tab切换
 const tabChange = (type) => {
   params.value.orderState = type
+  getOrderList()
+}
+
+// 页数切换
+const pageChange = (page) => {
+  params.value.page = page
   getOrderList()
 }
 </script>
@@ -114,7 +122,7 @@ const tabChange = (type) => {
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination :total="total" :page-size="params.pageSize" @current-change="pageChange" background layout="prev, pager, next" />
           </div>
         </div>
       </div>
